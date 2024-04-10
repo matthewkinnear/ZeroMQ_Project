deffb292263fe69c41afa3bf143f0ea6a7e4ff1b
matthewkinnear <mattmkinn@gmail.com>
2024-04-10 20:32:26 +0100

    chore: pyzmq server and client example

---
+++ utils/client.py
@@ -0,0 +1,16 @@
+import json
+
+import zmq
+
+
+def create_connection():
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
+    return socket
+
+
+def send_message(socket, message):
+    message_bytes = json.dumps(message).encode('utf-8')
+    print(f"Sending {message}")
+    socket.send(message_bytes)
