a05edcc5b40fa0ab9231d73f674b2b84ca5457be
matthewkinnear <mattmkinn@gmail.com>
2024-04-10 20:32:46 +0100

    feat: add example usage

---
+++ examples/example_1.py
@@ -0,0 +1,18 @@
+from utils.client import create_connection, send_message
+from utils.random_user import get_random_user
+
+
+def example_usage():
+    count = 0
+
+    while count < 10:
+        random_user = get_random_user()
+        socket = create_connection()
+        with socket:
+            send_message(socket, random_user)
+        count += 1
+    return
+
+
+if __name__ == "__main__":
+    example_usage()
